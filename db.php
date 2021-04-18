dab1dcf1f6774c44704673739591822da5e7282a
miwanoff <marina.iva.noffa@gmail.com>
2021-04-18 14:16:29 +0300

    add countries to db

--- db.php
+++ db.php
@@ -4,3 +4,33 @@ $users = [
     "user" => "user",
     "admin" => "admin",
 ];
+
+$countries = [
+    [
+        "name" => "France",
+        "capital" => "Paris",
+        "area" => 640679,
+        "population" => [
+            "2000" => 59278000,
+            "2010" => 59278000,
+        ],
+    ],
+    [
+        "name" => "England",
+        "capital" => "London",
+        "area" => 130395,
+        "population" => [
+            "2000" => 58800000,
+            "2010" => 63200000,
+        ],
+    ],
+    [
+        "name" => "Deutschland",
+        "capital" => "Berlin",
+        "area" => 357021,
+        "population" => [
+            "2000" => 82260000,
+            "2010" => 81752000,
+        ],
+    ],
+];
