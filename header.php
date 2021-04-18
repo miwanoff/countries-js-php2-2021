010dd32a2a2f62b9aba9b936efb7f99f802e63cf
miwanoff <marina.iva.noffa@gmail.com>
2021-04-18 15:44:59 +0300

    add template

--- header.php
+++ header.php
@@ -5,11 +5,77 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap"
+        rel="stylesheet">
+
+    <title>Countries</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/templatemo-klassy-cafe.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <header>
-        <h1>Страны</h1>
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.html" class="logo">
+                            <img src="assets/images/klassy-logo.png" align="klassy cafe html template">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about">About</a></li>
+
+                            <!--
+                            <li class="submenu">
+                                <a href="javascript:;">Drop Down</a>
+                                <ul>
+                                    <li><a href="#">Drop Down Page 1</a></li>
+                                    <li><a href="#">Drop Down Page 2</a></li>
+                                    <li><a href="#">Drop Down Page 3</a></li>
+                                </ul>
+                            </li>
+                        -->
+                            <li class="scroll-to-section"><a href="#menu">Menu</a></li>
+                            <li class="scroll-to-section"><a href="#chefs">Chefs</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Features</a>
+                                <ul>
+                                    <li><a href="#">Features Page 1</a></li>
+                                    <li><a href="#">Features Page 2</a></li>
+                                    <li><a href="#">Features Page 3</a></li>
+                                    <li><a href="#">Features Page 4</a></li>
+                                </ul>
+                            </li>
+                            <!-- <li class=""><a rel="sponsored" href="https://templatemo.com" target="_blank">External URL</a></li> -->
+                            <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
     </header>
+    <!-- ***** Header Area End ***** -->
     <hr>
--- index.php
+++ index.php
@@ -15,6 +15,18 @@ if (isset($_POST["go"])) {
     }
 
 }
+$cont_start_12 = '<div class="container">
+<div class="row">
+    <div class="col-12">';
+$cont_start_row = '<div class="container">
+    <div class="row">';
+
+$cont_start_8 = '<div class="col-8">';
+$cont_start_4 = '<div class="col-4">';
+$cont_end = ' </div>
+    </div>
+</div>';
+echo $cont_start_12;
 $user_form = '<form action="index.php" method="post" name="autoForm">
 Логин: <input type="text" name="login">
 Пароль: <input type="password" name="pass">
@@ -22,6 +34,9 @@ $user_form = '<form action="index.php" method="post" name="autoForm">
 </form>';
 echo $user_form;
 
+echo $cont_end;
+echo $cont_start_row;
+echo $cont_start_4;
 $str_form_s = '<h3>Сортировать по:</h3>
 <form action="index.php" method="post" name="sort_form">
 <select name="sort" id="sort" size="1">
@@ -32,6 +47,7 @@ $str_form_s = '<h3>Сортировать по:</h3>
 <input type="submit" name="submit" value="OK">
 </form>';
 echo $str_form_s;
+echo "</div>";
 
 if (isset($_POST['sort'])) {
     $how_to_sort = $_POST['sort'];
@@ -41,9 +57,11 @@ if (isset($_POST['sort'])) {
 $out = out_sort();
 
 if (count($out) > 0) {
+    echo $cont_start_8;
     foreach ($out as $row) {
         echo $row;
     }
+    echo $cont_end;
 }
 
 $str_form_search = "<h3>Поиск:</h3>
